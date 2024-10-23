4860519318db0aec691b4bdde8769cd63b58d10d
fahimshariar28 <fahimshariar28@gmail.com>
2024-10-23 23:53:39 +0600

    config index file updated

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -7,7 +7,7 @@ export default {
   node_env: process.env.NODE_ENV,
   port: process.env.PORT,
   database_url: process.env.DATABASE_URL,
-  password_salt_: process.env.PASSWORD_SALT_ROUNDS,
+  password_salt: process.env.PASSWORD_SALT_ROUNDS,
   jwt_secret: process.env.JWT_ACCESS_TOKEN_SECRET,
   jwt_expires_in: process.env.JWT_ACCESS_EXPIRATION,
 };
