85f88f5acc14fb4dbc63e694e23e94461689ab80
fahimshariar28 <fahimshariar28@gmail.com>
2024-10-24 15:39:54 +0600

    authVerification middleware added

---
+++ src/app/middlewares/authVerification.ts
@@ -0,0 +1,37 @@
+import { NextFunction, Request, Response } from "express";
+import httpStatus from "http-status";
+import { verifyToken } from "../helpers/jwtHelper";
+import config from "../config";
+import JWTError from "../errors/jwtError";
+
+const authVerification =
+  (...requiredRoles: string[]) =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      //get authorization token
+      const token = req.headers.authorization;
+      if (!token) {
+        throw new JWTError(
+          httpStatus.UNAUTHORIZED,
+          "Authorization token is required",
+          null
+        );
+      }
+      // verify token
+      let verifiedUser = null;
+
+      verifiedUser = verifyToken(token, config.jwt_secret as string);
+
+      req.user = verifiedUser; // set user in request object
+
+      // role based access
+      if (requiredRoles.length && !requiredRoles.includes(verifiedUser.role)) {
+        throw new JWTError(httpStatus.FORBIDDEN, "You are not allowed", null);
+      }
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
+
+export default authVerification;
