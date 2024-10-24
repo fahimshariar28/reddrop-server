37944e8f2c94c647f34ef3facc355c9ffd3f1f87
fahimshariar28 <fahimshariar28@gmail.com>
2024-10-24 15:59:21 +0600

    auth route updated

--- src/app/modules/auth/auth.route.ts
+++ src/app/modules/auth/auth.route.ts
@@ -2,6 +2,8 @@ import express from "express";
 import validateZodRequest from "../../middlewares/validateZodRequest";
 import { AuthController } from "./auth.controller";
 import { authValidation } from "./auth.validation";
+import authVerification from "../../middlewares/authVerification";
+import { ROLE } from "../../enums/userEnum";
 
 // Route to login a user
 const router = express.Router();
@@ -15,9 +17,17 @@ router.post(
 router.post("/refresh", AuthController.refreshToken);
 
 // Route to set password
-router.put("/set-password/:id", AuthController.setPassword);
+router.put(
+  "/set-password/:id",
+  authVerification(ROLE.ADMIN, ROLE.USER),
+  AuthController.setPassword
+);
 
 // Route to change password
-router.put("/change-password/:id", AuthController.changePassword);
+router.put(
+  "/change-password/:id",
+  authVerification(ROLE.ADMIN, ROLE.USER),
+  AuthController.changePassword
+);
 
 export const AuthRoutes = router;
