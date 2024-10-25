0ac72e2904e4e50c66f1f6c71d7102cd84a25994
fahimshariar28 <fahimshariar28@gmail.com>
2024-10-25 23:58:30 +0600

    auth route updated

--- src/app/modules/auth/auth.route.ts
+++ src/app/modules/auth/auth.route.ts
@@ -1,17 +1,11 @@
 import express from "express";
-import validateZodRequest from "../../middlewares/validateZodRequest";
 import { AuthController } from "./auth.controller";
-import { authValidation } from "./auth.validation";
 import authVerification from "../../middlewares/authVerification";
 import { ROLE } from "../../enums/userEnum";
 
 // Route to login a user
 const router = express.Router();
-router.post(
-  "/login",
-  validateZodRequest(authValidation.loginValidationSchema),
-  AuthController.loginUser
-);
+router.post("/login", AuthController.loginUser);
 
 // Route to refresh the token
 router.post("/refresh", AuthController.refreshToken);
