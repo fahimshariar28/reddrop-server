af3acf5dd24031b18cac3de0437443fc94fea031
fahimshariar28 <fahimshariar28@gmail.com>
2024-10-27 19:44:30 +0600

    socket.io implementation in server

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,49 @@
 import app from "./app";
 import mongoose from "mongoose";
 import config from "./app/config";
+import http from "http";
+import { Server } from "socket.io";
+import { UserService } from "./app/modules/user/user.service";
 
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
 
-    app.listen(config.port, () => {
+    // Create an HTTP server to use with Socket.io
+    const httpServer = http.createServer(app);
+    const io = new Server(httpServer, {
+      cors: {
+        origin: "*", // TODO: Change this to the frontend URL in production
+        methods: ["GET", "POST"],
+      },
+    });
+
+    io.on("connection", (socket) => {
+      console.log(`User connected: ${socket.id}`);
+
+      const userId = socket.handshake.query.userId as string;
+
+      // Set user as active upon connection
+      if (userId) {
+        console.log(`User ${userId} is active.`);
+        UserService.setUserActive(userId);
+      }
+
+      socket.on("disconnect", () => {
+        // Set user as inactive upon disconnection
+        if (userId) {
+          UserService.setUserInactive(userId);
+          console.log(`User ${userId} is inactive.`);
+        }
+      });
+
+      // Handle user disconnecting
+      socket.on("disconnect", () => {
+        console.log(`User disconnected: ${socket.id}`);
+      });
+    });
+
+    httpServer.listen(config.port, () => {
       console.log(`RoktoBondhu Server is running on port ${config.port}.`);
     });
   } catch (error) {
