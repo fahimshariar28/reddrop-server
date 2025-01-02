134e5cd517cf153cb2bbd78e11e7c791e05070a6
fahimshariar28 <fahimshariar28@gmail.com>
2025-01-03 02:14:45 +0600

    app.ts file modified

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,12 @@ const app: Application = express();
 
 // Parsers
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
