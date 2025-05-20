faf13e305ff87b3973434c4207256455d07b9d5f
Fahim <fahimshariar28@gmail.com>
2025-05-20 17:55:56 +0600

    add front_end_url to configuration

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -6,6 +6,7 @@ dotenv.config({ path: path.join(process.cwd(), ".env") });
 export default {
   node_env: process.env.NODE_ENV,
   port: process.env.PORT,
+  front_end_url: process.env.FRONTEND_URL,
   database_url: process.env.DATABASE_URL,
   password_salt: process.env.PASSWORD_SALT_ROUNDS,
   jwt_secret: process.env.JWT_ACCESS_TOKEN_SECRET,
